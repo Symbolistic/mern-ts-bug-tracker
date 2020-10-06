7945dd5174f1d342c27849cccf5cf9d03baca932
Symbol <Symbolistic@live.com>
2020-10-06 18:42:30 -0400

    fixed a notification css issue that happened on smaller screens and added to the support page

--- client/src/components/Support/Support.tsx
+++ client/src/components/Support/Support.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navbar } from '../Navbar/Navbar';
 import { Sidebar } from '../Sidebar/Sidebar';
-import { Container } from '@material-ui/core';
+import { Container, TextField } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 
 interface Props {}
@@ -21,15 +21,37 @@ export const Support: React.FC<Props> = () => {
 							<Grid item xs={12} md={12} lg={12} className='header'>
 								<h2>Support</h2>
 								<p>
-									Use this page to contact support or to send us a message
-									directly
+									Use this page to contact support or to send a message directly
 								</p>
 							</Grid>
 						</Grid>
 
 						<Grid item xs={12} md={12} lg={12}>
-							<h3>Still being worked on...</h3>
-							<p>Come back another time</p>
+							<h3>Contact:</h3>
+							<form>
+								<TextField label='Name' />
+								<br />
+								<br />
+
+								<TextField label='Email' />
+								<br />
+								<br />
+
+								<TextField
+									id='outlined-multiline-static'
+									multiline
+									rows={10}
+									placeholder='Enter your message here'
+									variant='outlined'
+									label='Message'
+									size='medium'
+									style={{ width: 260, marginLeft: '-5px' }}
+								/>
+
+								<br />
+								<br />
+								<button className='btn'>Submit</button>
+							</form>
 						</Grid>
 					</Container>
 				</Grid>
